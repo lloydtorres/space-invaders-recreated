ba5b8a43024deb721f2c5bbc42191ec1f096fcbf
Dovydas Venckus <dovven2@ktu.lt>
2023-11-06 23:04:40 +0200

    Implements prototype pattern for EnemyServerEntity

--- src/server/entities/EnemyServerEntity.java
+++ src/server/entities/EnemyServerEntity.java
@@ -1,12 +1,18 @@
 package server.entities;
 
 import common.EntityType;
-
-public class EnemyServerEntity extends ServerEntity {
+ 
+public class EnemyServerEntity extends ServerEntity implements Cloneable {
     private int pointWorth;
+
     public EnemyServerEntity(float X, float Y, int pointWorth){
         super(EntityType.ENEMY, X, Y, 37, 24, 18, 24);
         this.pointWorth = pointWorth;
+    }
+
+    public EnemyServerEntity(EnemyServerEntity e){
+        super(EntityType.ENEMY, e.getX(), e.getY(), e.getWidth(), e.getHeight(), e.getXSpeed(), e.getYSpeed());
+        this.pointWorth = e.pointWorth;
     }
 
     public void setPointWorth(int pointWorth) {
@@ -15,5 +21,18 @@ public class EnemyServerEntity extends ServerEntity {
 
     public int getPointWorth() {
         return pointWorth;
+    }
+
+    public EnemyServerEntity shallowCopy(){
+        try{
+            return (EnemyServerEntity) this.clone();
+        } catch(CloneNotSupportedException ex){
+            ex.printStackTrace();
+            return this;
+        }
+    }
+    
+    public EnemyServerEntity deepCopy(){
+        return new EnemyServerEntity(this);
     }
 }
