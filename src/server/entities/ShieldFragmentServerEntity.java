41c5edaf2f43c890d170c5868a6900eee7bd9a36
Martynas <martynas.gudziunas@gmail.com>
2023-11-21 00:05:30 +0200

    Small fix to prototype

--- src/server/entities/ShieldFragmentServerEntity.java
+++ src/server/entities/ShieldFragmentServerEntity.java
@@ -2,44 +2,24 @@ package server.entities;
 
 import common.EntityType;
 
-class Placeholder implements Cloneable{
-    String text;
-    int number;
-
-    public Placeholder(String text, int number){
-        this.text = text;
-        this.number = number;
-    }
-
-    @Override
-    public Object clone(){
-        try{
-            return (Placeholder) super.clone();
-        }catch(CloneNotSupportedException ex){
-            ex.printStackTrace();
-            return new Placeholder(this.text, this.number);
-        }
-    }
-}
-
-public class ShieldFragmentServerEntity extends ServerEntity implements Cloneable{
+public class ShieldFragmentServerEntity extends ServerEntity implements Cloneable {
     public Placeholder placeholder;
 
-    public ShieldFragmentServerEntity(float X, float Y, String text, int number) {
+    public ShieldFragmentServerEntity(float X, float Y, Placeholder placeholder) {
         super(EntityType.SHIELD, X, Y, 10, 10, 0, 0);
-        placeholder = new Placeholder(text, number);
+        this.placeholder = placeholder;
     }
 
-    public ShieldFragmentServerEntity shallowCopy(){
-        try{
+    public ShieldFragmentServerEntity shallowCopy() {
+        try {
             return (ShieldFragmentServerEntity) this.clone();
-        } catch(CloneNotSupportedException ex){
+        } catch (CloneNotSupportedException ex) {
             ex.printStackTrace();
-            return new ShieldFragmentServerEntity(this.getX(), this.getY(), this.placeholder.text, this.placeholder.number);
+            return new ShieldFragmentServerEntity(this.getX(), this.getY(), this.placeholder);
         }
     }
 
-    public ShieldFragmentServerEntity deepCopy(){
+    public ShieldFragmentServerEntity deepCopy() {
         ShieldFragmentServerEntity copy = this.shallowCopy();
         copy.placeholder = (Placeholder) this.placeholder.clone();
         return copy;
